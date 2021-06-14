50bd203da2fed7fcb1c51daa2a303b3f48feb315
maicol <maicol.matteoli@gmail.com>
2021-06-14 14:14:27 +0200

    test : push dell'oggetto in un array vuoto

--- js/script.js
+++ js/script.js
@@ -8,3 +8,9 @@ for(var k in studente){
     // k identifica il nome della proprietà mentre studente[k] identifica il valore 
     console.log(k , " : ", studente[k]); //visualizza tutte le proprietà dell'oggetto
 }
+
+var studenti = [];
+
+studenti.push(studente);
+console.log(studenti);
+

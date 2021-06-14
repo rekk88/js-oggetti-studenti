e156f9af10c18dc3ecebb8b5646f27a404d7557e
maicol <maicol.matteoli@gmail.com>
2021-06-14 14:11:25 +0200

    add : visualizzazione propietà dell'oggetto studente

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var studente = {
+    nome : "gianni",
+    cognome  : "g",
+    età : 15,
+};
+
+for(var k in studente){
+    // k identifica il nome della proprietà mentre studente[k] identifica il valore 
+    console.log(k , " : ", studente[k]); //visualizza tutte le proprietà dell'oggetto
+}
